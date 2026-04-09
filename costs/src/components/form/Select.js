598d114b0a07e0af497ea449fc00a8fbd991931d
Natan Gabriel Lopes <naglopes2007@gmail.com>
2026-04-09 10:25:10 -0300

    Create database and seed categories

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -4,6 +4,9 @@ function Select({ text, name, options, handleOnChange, value}){
             <label className="font-medium text-gray-800" htmlFor={name}>{text}:</label>
             <select  name={name} id={name} className="border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent">
                 <option className="text-gray-800">Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ btnText }) {
+
+    const[categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers:{
+            "Content-Type": "application/json"
+        },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data);
+    })
+    .catch((err) => window.alert(err));
+    }, []);
+
     return (
         <form className="flex flex-col gap-5">
             <Input 
@@ -17,7 +35,7 @@ function ProjectForm({ btnText }) {
                 name="budget" 
                 placeholder="Digite o orçamento total" 
             />
-            <Select name='category_id' text='Selecione a categoria'/>
+            <Select name='category_id' text='Selecione a categoria' options={categories}/>
              <SubmitButton text={btnText}/>
         </form>
     )
