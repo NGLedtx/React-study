d6b8124c5fe5c534c4f8d009f13ce52d1aafc9b9
Natan Gabriel Lopes <naglopes2007@gmail.com>
2026-04-27 21:32:08 -0300

    feat: add new input area for service description

---
+++ costs/src/components/form/TextArea.js
@@ -0,0 +1,15 @@
+function TextArea({text, name, placeholder, handleOnChange, value }) {
+    return(
+        <div className="flex flex-col gap-2 my-0.5">
+            <label className="font-medium text-gray-800" htmlFor={name}>{text}:</label>
+            <textarea className="border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent h-20"
+                id={name} 
+                name={name} 
+                placeholder={placeholder} 
+                onChange={handleOnChange} 
+                value={value}
+            />
+        </div>
+    )
+}
+export default TextArea;
