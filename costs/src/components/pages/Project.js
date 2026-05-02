2c67f735a776b008f3145abcc33e35d3d3f22196
Natan Gabriel Lopes <naglopes2007@gmail.com>
2026-05-02 11:08:43 -0300

    feat: improve project page UI and functionality

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -2,13 +2,17 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ProjectForm from "../project/ProjectForm";
 import ServiceForm from "../project/ServiceForm";
+import ServiceCard from "../project/ServiceCard";
 import Loading from "../layout/Loading";
 import Message from "../layout/Message";
-import {parse, v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid";
+import Modal from "../layout/Modal";
+import { BsPencilSquare } from "react-icons/bs";
 function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState(null);
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
   const [showServiceForm, setShowServiceForm] = useState(false);
   const [showMessage, setShowMessage] = useState();
@@ -24,6 +28,7 @@ function Project() {
       .then((resp) => resp.json())
       .then((data) => {
         setProject(data);
+        setServices(data.services);
       })
       .catch((err) => window.alert(err));
   }, [id]);
@@ -56,21 +61,25 @@ function Project() {
   }
 
   function createService(project) {
-    setShowMessage("")
-    const lastService = project.services[project.services.length - 1]
-    lastService.id = uuidv4()
+    setShowMessage("");
+    if (!project.services || project.services.length === 0) {
+      return;
+    }
+    const lastService = project.services[project.services.length - 1];
+    lastService.id = uuidv4();
 
-    const lastServiceCost = lastService.cost
-    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+    const lastServiceCost = lastService.cost;
+    const newCost =
+      parseFloat(project.cost || 0) + parseFloat(lastServiceCost || 0);
 
-    if(newCost > parseFloat(project.budget)){
-      setShowMessage('Orçamento ultrapassado, verifique o valor do serviço')
-      setType('error')
-      project.services.pop()
-      return false
+    if (newCost > parseFloat(project.budget)) {
+      setShowMessage("Orçamento ultrapassado, verifique o valor do serviço");
+      setType("error");
+      project.services.pop();
+      return false;
     }
 
-    project.cost = newCost
+    project.cost = newCost;
 
     fetch(`http://localhost:5000/projects/${project.id}`, {
       method: "PATCH",
@@ -78,14 +87,37 @@ function Project() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(project),
-      }).then((resp) => resp.json())
+    })
+      .then((resp) => resp.json())
       .then((data) => {
-
+        setShowServiceForm(false);
       })
       .catch((err) => window.alert(err));
-
   }
+  function removeService(id, costs) {
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id,
+    );
+    const projectUpdated = project;
+    projectUpdated.services = servicesUpdated;
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(costs);
 
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectUpdated),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(projectUpdated);
+        setServices(servicesUpdated);
+        setShowMessage("Serviço removido com sucesso!");
+        setType("success");
+      })
+      .catch((err) => window.alert(err));
+  }
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
   }
@@ -103,66 +135,70 @@ function Project() {
           onClick={toggleProjectForm}
           className="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-800 "
         >
-          {!showProjectForm ? "Editar Projeto" : "Fechar Formulário"}
+          <BsPencilSquare />
         </button>
       </div>
-      {!showProjectForm ? (
-        <div className="flex flex-col my-4 border-b-2 shadow-md text-lg pb-4 gap-3 p-5">
-          <div className="flex ">
-            <span>
-              <b>Categoria:</b> {project.category.name}
-            </span>
-          </div>
-          <div className="flex ">
-            <span>
-              <b>Total de Orçamento:</b> R${project.budget},00
-            </span>
-          </div>
-          <div className="flex ">
-            <span>
-              <b>Gasto:</b> R${project.cost},00
-            </span>
-          </div>
+      <div className="flex flex-col my-4 border-b-2 shadow-md text-lg pb-4 gap-3 p-5">
+        <div className="flex ">
+          <span>
+            <b>Categoria:</b> {project.category.name}
+          </span>
         </div>
-      ) : (
-        <div className="flex flex-col my-4">
-          <div className="mb-5">
-            <h2 className="text-xl font-bold">Formulário de Edição</h2>
-            <p className="text-gray-500">
-              Aqui você pode editar os detalhes do projeto.
-            </p>
-          </div>
+        <div className="flex ">
+          <span>
+            <b>Total de Orçamento:</b> R${project.budget},00
+          </span>
+        </div>
+        <div className="flex ">
+          <span>
+            <b>Gasto:</b> R${project.cost},00
+          </span>
+        </div>
+      </div>
+      {showProjectForm && (
+        <Modal onClose={toggleProjectForm}>
           <ProjectForm
             handleSubmit={editPost}
-            btnText="Concluir Edição"
+            btnText={"Concluir Edição"}
             projectData={project}
           />
-        </div>
+        </Modal>
       )}
-      <div className="flex flex-col my-5">
-        <div className="flex justify-between border-b-2 shadow-md gap-3 my-6 p-3">
-          <h2 className="text-2xl font-bold">Adicione um serviço:</h2>
+      <div className="border-b-2 shadow-md gap-3 my-6 p-5 h-auto">
+        <div className="flex justify-between w-full mb-5">
+          <h1 className="text-2xl font-bold">Serviços</h1>
           <button
             onClick={toggleServiceForm}
             className="bg-gray-900 text-white py-2 px-4 rounded hover:bg-gray-800 "
           >
-            {!showServiceForm ? "Adicionar serviço" : "Fechar Formulário"}
+            + Adicionar Serviço
           </button>
         </div>
-        <div>
-          {showServiceForm && (
-            <div className="p-5 rounded-md">
-              <ServiceForm
-                handleSubmit={createService}
-                btnText={"Adicionar Serviço"}
-                projectData={project}
-              />
-            </div>
-          )}
-        </div>
-        <div className="border-b-2 shadow-md gap-3 my-6">
-          <h1 className="text-2xl font-bold" >Serviços</h1>
-        </div>
+        {showServiceForm && (
+          <Modal onClose={toggleServiceForm}>
+            <ServiceForm
+              handleSubmit={createService}
+              btnText={"Confirmar!"}
+              projectData={project}
+            />
+          </Modal>
+        )}
+        {services.length > 0 &&
+          services.map((service) => (
+            <ServiceCard
+              id={service.id}
+              name={service.name}
+              cost={service.cost}
+              description={service.description}
+              key={service.id}
+              handleRemove={removeService}
+            />
+          ))}
+        {services.length === 0 && (
+          <p className="text-gray-500 text-center">
+            Não há serviços cadastrados.
+          </p>
+        )}
       </div>
     </div>
   );
